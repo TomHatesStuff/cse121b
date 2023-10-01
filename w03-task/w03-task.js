e5b5b4c808aee57f68a93e29e559f1ce3cd8cf82
TomHatesStuff <tlukemarshall1@gmail.com>
2023-09-30 18:09:05 -0600

    test

--- w03-task/w03-task.js
+++ w03-task/w03-task.js
@@ -1,9 +1,20 @@
 /* LESSON 3 - Programming Tasks */
 
 /* FUNCTIONS */
+function add(number1, number2) 
+{
+    return number1 + number2;
+}
 /* Function Definition - Add Numbers */
 
+function AddNumbers ()
+{
+let AddNumber1 = Number (document.querySelector("#add1").value)
+let AddNumber2 = Number (document.querySelector("#add2").value)
 
+document.querySelector ("#sum").value = add(AddNumber1, AddNumber2);
+}
+document.querySelector("#AddNumbers").addEventListener("click", AddNumbers)
 /* Function Expression - Subtract Numbers */
 
 
