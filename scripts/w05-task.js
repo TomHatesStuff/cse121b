678692b742727fdb322cdba6271d4ebba03e77b7
TomHatesStuff <tlukemarshall1@gmail.com>
2023-10-14 10:36:52 -0600

    sd

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -1,25 +1,21 @@
 /* W05: Programming Tasks */
 
-// Declare a const variable named templesElement
+// Step 1: Declare a const variable named templesElement
 const templesElement = document.getElementById("temples");
 
-// Declare a global empty array variable to store a list of temples
-let templeList = [];
+// Step 2: Declare a global empty array variable to store a list of temples
+const templeList = [];
 
-// Declare a function expression using const named displayTemples
+// Step 3: Declare a function expression named displayTemples
 const displayTemples = (temples) => {
-  // Clear existing temple data
-  templesElement.innerHTML = "";
+  // Clear existing content
+  templesElement.innerHTML = '';
 
-  // Create a container for the row
-  let rowContainer = document.createElement("div");
-  rowContainer.classList.add("row");
-
-  temples.forEach((temple, index) => {
+  temples.forEach((temple) => {
     // Create an HTML <article> element
     const article = document.createElement("article");
 
-    // Create an HTML <h3> element and add the temple's templeName property
+    // Create an HTML <h3> element
     const h3 = document.createElement("h3");
     h3.textContent = temple.templeName;
 
@@ -32,41 +28,64 @@ const displayTemples = (temples) => {
     article.appendChild(h3);
     article.appendChild(img);
 
-    // Append the <article> element to the current row
-    rowContainer.appendChild(article);
-
-    // If we've reached three items or the end of the list, create a new row
-    if ((index + 1) % 3 === 0 || index === temples.length - 1) {
-      templesElement.appendChild(rowContainer);
-      rowContainer = document.createElement("div");
-      rowContainer.classList.add("row");
-    }
+    // Append the <article> element to the global templesElement
+    templesElement.appendChild(article);
   });
 };
 
-// Create a function expression called getTemples (async)
+// Step 4: Declare a function expression named getTemples
 const getTemples = async () => {
   try {
-    // Fetch temple data from the provided URL
     const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
     const templeData = await response.json();
+    templeList.push(...templeData);
 
-    // Assign the result to the templeList global array variable
-    templeList = templeData;
-
-    // Call the displayTemples function with the templeList
+    // Call the displayTemples function and pass it the list of temples (templeList)
     displayTemples(templeList);
   } catch (error) {
-    console.error("Error fetching temple data:", error);
+    console.error("An error occurred while fetching temple data:", error);
   }
 };
 
-// Add a change event listener to the select element with the ID of sortBy
+// Step 5: Declare a function expression named reset
+const reset = () => {
+  templesElement.innerHTML = '';
+};
+
+// Step 6: Declare a function expression named sortBy
+const sortBy = (temples) => {
+  // Call the reset function to clear the output
+  reset();
+
+  // Define a variable named filter that obtains the value of the HTML element with the ID of sortBy
+  const filter = document.getElementById("sortBy").value;
+
+  // Use a switch statement to filter the temples parameter based on the selected option
+  switch (filter) {
+    case "utah":
+      displayTemples(temples.filter(temple => temple.location.includes("Utah")));
+      break;
+    case "notutah":
+      displayTemples(temples.filter(temple => !temple.location.includes("Utah")));
+      break;
+    case "older":
+      displayTemples(temples.filter(temple => new Date(temple.dedicated) <= new Date("1950-01-01")));
+      break;
+    case "all":
+      displayTemples(temples);
+      break;
+  }
+};
+
+// Add a change event listener to the HTML element with an ID of sortBy
 document.getElementById("sortBy").addEventListener("change", () => {
+  // Call the sortBy function and send an arrow function result with the templeList as the argument
   sortBy(templeList);
 });
 
-// Call the getTemples function to initially populate the temple data
+// Call getTemples to fetch and display temple data initially
 getTemples();
+
+
 
 /* Event Listener */
